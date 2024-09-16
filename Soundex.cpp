ac5e7f1178067387c092ec4d72768d84bc808ccc
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-16 10:54:23 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,1 +1,16 @@
+#include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
+
+int isEmpty(const char*inputString)
+{
+if(strlen(inputString)==0)
+  return 1;
+}
+  
+void generateSoundex (const char*inputString, const char*SoundexCode){
+if(isEmpty){
+  strcpy(Soundex[0] , '\0');
+    }
+
 
