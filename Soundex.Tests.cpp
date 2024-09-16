0867409d4260283cd30ed94bd5e2da356e4799fa
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-16 10:47:23 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,1 +1,9 @@
-
+#include <gtest/gtest.h>
+#include "Soundex.h"
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+//AAA
+  char soundex[5];
+  generateSoundex(" ", soundex);
+  ASSERT_EQ(soundex," ");
+}
