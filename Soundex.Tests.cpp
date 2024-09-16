a167e96fedd057cd7e86385a42e25b7a1941f728
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-16 10:17:59 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,19 +1,1 @@
-#include <gtest/gtest.h>
-#include "Soundex.h"
- 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
-//AAA
-  char soundex[5];
-  generateSoundex("Robert", soundex);
-  ASSERT_STREQ(soundex,"R163");
-  generateSoundex("ac", soundex);
-  ASSERT_STREQ(soundex,"A200");
-  generateSoundex("ad", soundex);
-  ASSERT_STREQ(soundex,"A300");
-  generateSoundex("af", soundex);
-  ASSERT_STREQ(soundex,"A100");
-  ASSERT_STREQ(soundex,"A100");
-// ASSERT_STREQ("Robert", "R163");
- 
-}
 
